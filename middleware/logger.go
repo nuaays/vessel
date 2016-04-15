bb4be5736dbecf6255dff62c6ef280ad56c39203
mengfanliang <mengfanliang@huawei.com>
2016-04-15 22:30:07 +0800

    C-0

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,33 +1,19 @@
 package middleware
 
 import (
-	//"github.com/ngaut/log"
-	//	"github.com/containerops/vessel/module"
 
 	"gopkg.in/macaron.v1"
 )
 
 func InitLog(runmode, path string) {
-	//	log := module.GetLog()
-
-	//	log.SetLevelByString("info")
-
 	if runmode == "dev" {
-		//		log.SetLevelByString("debug")
 	}
 
-	//	log.SetOutputByName(path)
 }
 
 func logger(runmode string) macaron.Handler {
-	//	log := module.GetLog()
 	return func(ctx *macaron.Context) {
 		if runmode == "dev" {
-			//			log.Debug("")
-			//			log.Debug("----------------------------------------------------------------------------------")
 		}
-
-		//		log.Infof("[%s] [%s]", ctx.Req.Method, ctx.Req.RequestURI)
-		//		log.Infof("[Header] %v", ctx.Req.Header)
 	}
 }
--- models/pipeline.go
+++ models/pipeline.go
@@ -1,18 +1,5 @@
 package models
 
-const (
-	PIPESUCCESS = iota
-	PIPEERROR
-	PIPERUNNING
-	PIPEPENDDING
-	PIPEEXCEPT
-)
-
-var (
-	DEFAULT_PIPELINE_ETCD_PATH        = "/containerops/vessel/ws-%d/pj-%d/pl-%d/stage/"
-	DEFAULT_PIPELINEVERSION_ETCD_PATH = "/containerops/vessel/ws-%d/pj-%d/plv-%d/stagev-%d/"
-)
-
 type Pipeline struct {
 	Id          int64  `json:"id"`
 	WorkspaceId int64  `json:"workspaceId"`
@@ -43,60 +30,7 @@ type PipelineVersion struct {
 	Detail        string   `json:"detail" gorm:"type:text"`
 	StageVersions []string `json:"stageVersions"`
 	Log           string   `json:"log" gorm:"type:text"`
-	Status        int64    `json:"state"` // 0 not start    1 working    2 success     3 failed
+	Status        int64    `json:"state"`
 	MetaData      PipelineMetaData
 	StageSpecs    []StageSpec
-}
-
-func (pv *PipelineVersion) GetMetadata() PipelineMetaData {
-	return pv.MetaData
-}
-
-func (pv *PipelineVersion) GetSpec() []StageSpec {
-	return pv.StageSpecs
-}
-
-// type Status struct {
-// 	Id         int64     `json:"id"`                                        //
-// 	PipelineId int64     `json:"pipelineId"`                                //
-// 	UUID       string    `json:"uuid" orm:"varchar(255)"`                   //
-// 	Resource   string    `json:"resrouce" orm:"null;type(text)"`            //
-// 	ActionId   string    `json:"actionId" orm:"unique;varchar(255)"`        // Point.UUID; Stage.UUID
-// 	Started    time.Time `json:"started" orm:"type(datetime)"`              //
-// 	Ended      time.Time `json:"ended" orm:"type(datetime)"`                //
-// 	Log        string    `json:"log" orm:"type(text)"`                      //
-// 	Result     int64     `json:"result" orm:"null"`                         // Success: 0; Failure: 1
-// 	Actived    bool      `json:"actived" orm:"null;default(0)"`             //
-// 	Created    time.Time `json:"created" orm:"auto_now_add;type(datetime)"` //
-// 	Updated    time.Time `json:"updated" orm:"auto_now;type(datetime)"`     //
-// 	Memo       string    `json:"memo" orm:"null;type(text)"`                //
-// }
-
-//Create Pipeline
-func (pipe *Pipeline) Create(projectId int64, name string) (error, int64) {
-	return nil, 0
-}
-
-//List all run history with pipelineId
-func (pipe *Pipeline) History(pipelineId int64) (error, []string) {
-	return nil, []string{}
-}
-
-//Return all point and stage status with status uuid
-func (pipe *Pipeline) Status(uuid string) (error, []int64) {
-	return nil, []int64{}
-}
-
-//Stop run
-func (pipe *Pipeline) Stop(uuid string) error {
-	return nil
-}
-
-//Clean run resources
-func (pipe *Pipeline) Clean(uuid string) error {
-	return nil
-}
-
-func (pipe *Pipeline) Copy(uuid string) (error, string) {
-	return nil, ""
 }
